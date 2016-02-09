905bb7deca511214775bd6effcf536697df6dc27
jeffgrunewald <jeff@grunewalddesign.com>
2016-02-08 22:15:10 -0500

    initial commit

---
+++ app/scripts/services.js
@@ -0,0 +1,46 @@
+'use strict';
+
+angular.module('confusionApp')
+        .constant("baseURL", "http://localhost:3000/")
+
+        .service('menuFactory', [ '$resource', 'baseURL', function($resource, baseURL) {
+
+                this.getDishes = function(){
+
+                    return $resource(baseURL + "dishes/:id", null, {'update':{method: 'PUT'}});
+
+                };
+
+                this.getPromotion = function() {
+
+                    return $resource(baseURL + "promotions/:id", null);
+                };
+
+
+        }])
+
+        .factory('corporateFactory', [ '$resource', 'baseURL', function($resource, baseURL) {
+
+            var corpfac = {};
+
+            corpfac.getLeaders = function () {
+                return $resource(baseURL + "leadership/:id", null);
+            };
+
+            return corpfac;
+
+        }])
+
+        .factory('feedbackFactory', [ '$resource', 'baseURL', function($resource, baseURL) {
+
+            var feedbackfac = {};
+
+            feedbackfac.getFeedback = function() {
+                return $resource(baseURL + "feedback/:id", null);
+            };
+
+            return feedbackfac;
+
+        }])
+
+;
